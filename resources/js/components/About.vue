7a8fc58ca8c32a8f11c70d4a46bbfab75d8900d6
fauzi-ardi <fauzi.ardiantama@gmail.com>
2024-01-22 12:07:38 +0700

    frontend with vue and vue route done

---
+++ resources/js/components/About.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <h1>About</h1>
+                <p>This is the about page</p>
+                <!-- make a vue link to route / -->
+                <router-link to="/">Home</router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('About component mounted.')
+        }
+    }
+</script>
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <h1>Home</h1>
+                <p>This is the home page</p>
+                <!-- make a vue link to route /about -->
+                <router-link to="/about">About</router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Home component mounted.')
+        }
+    }
+</script>
